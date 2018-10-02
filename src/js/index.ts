cd99a52206c61c9dc2dd8fe63beebe9878bfde12
serubi <serubi@live.com>
2018-10-02 12:59:18 +0200

    Tilføj vare og hide på klik

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,30 @@
 //Her skal du skrive din typescriptkode
+
+document.getElementById("addProduct").addEventListener('click', addProduct);
+
+let sundtItems = document.getElementsByClassName('sundt');
+let usundtItems = document.getElementsByClassName('usundt');
+
+for (let i = 0; i < sundtItems.length; i++) {
+  sundtItems[i].addEventListener('click', hideElement);
+}
+
+for (let i = 0; i < usundtItems.length; i++) {
+  usundtItems[i].addEventListener('click', hideElement);
+}
+
+function addProduct() {
+  let inputText = (<HTMLInputElement>document.getElementById('addProductText')).value;
+  let list = document.getElementById('liste');
+  let newLi = document.createElement('li');
+  let newText = document.createTextNode(inputText);
+
+  newLi.setAttribute('class', 'sundt');
+  newLi.appendChild(newText);
+  list.appendChild(newLi);
+  newLi.addEventListener('click', hideElement);
+}
+
+function hideElement() {
+  this.setAttribute('hidden', 'true');
+}
