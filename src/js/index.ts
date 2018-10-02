2d4e9c6b3a1d0cb4508167e41aa47c2f3fcdff5c
serubi <serubi@live.com>
2018-10-02 14:41:43 +0200

    Tilføjet types

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,12 @@
 //Her skal du skrive din typescriptkode
 
-document.getElementById("addProduct").addEventListener('click', addProduct);
+document.getElementById("addProduct").addEventListener('click', function () {
+  addProduct(true);
+}, false);
+
+document.getElementById("addProductU").addEventListener('click', function () {
+  addProduct(false);
+}, false);
 
 let sundtItems = document.getElementsByClassName('sundt');
 let usundtItems = document.getElementsByClassName('usundt');
@@ -13,18 +19,44 @@ for (let i = 0; i < usundtItems.length; i++) {
   usundtItems[i].addEventListener('click', hideElement);
 }
 
-function addProduct() {
-  let inputText = (<HTMLInputElement>document.getElementById('addProductText')).value;
-  let list = document.getElementById('liste');
-  let newLi = document.createElement('li');
-  let newText = document.createTextNode(inputText);
+function addProduct(sundt: boolean) {
+  let inputText: string = (<HTMLInputElement>document.getElementById('addProductText')).value;
+  let list: HTMLUListElement = <HTMLUListElement>document.getElementById(sundt ? 'liste' : 'listeUsund');
+  let newLi: HTMLLIElement = createListItem(inputText, sundt, list.children.length);
 
-  newLi.setAttribute('class', 'sundt');
-  newLi.appendChild(newText);
   list.appendChild(newLi);
   newLi.addEventListener('click', hideElement);
 }
 
 function hideElement() {
   this.setAttribute('hidden', 'true');
+}
+
+function createListItem(liText: string, sundt: boolean, liIndex: Number): HTMLLIElement {
+  let newLi: HTMLLIElement = document.createElement('li');
+  let newText: Text = document.createTextNode(liText);
+  newLi.setAttribute('class', sundt ? 'sundt' : 'usundt');
+  newLi.setAttribute('id', liIndex.toString());
+  newLi.appendChild(newText);
+  return newLi;
+}
+
+let arrayOfStrings: string[] = ["string1", "string2", "string3", "string4", "string5"];
+
+for (let i = 0; i < arrayOfStrings.length; i++) {
+  let sundt: boolean = true;
+  let list: HTMLUListElement = <HTMLUListElement>document.getElementById(sundt ? 'liste' : 'listeUsund');
+  let newLi: HTMLLIElement = createListItem(arrayOfStrings[i], sundt, i);
+
+  list.appendChild(newLi);
+  newLi.addEventListener('click', hideElement);
+}
+
+for (let i = 0; i < arrayOfStrings.length; i++) {
+  let sundt: boolean = false;
+  let list: HTMLUListElement = <HTMLUListElement>document.getElementById(sundt ? 'liste' : 'listeUsund');
+  let newLi: HTMLLIElement = createListItem(arrayOfStrings[i], sundt, i);
+
+  list.appendChild(newLi);
+  newLi.addEventListener('click', hideElement);
 }
